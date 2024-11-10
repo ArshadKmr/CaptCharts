f706d355405e1ca4ed77756b8dd2f4e2f780f35e
muhammedarshadnorq <muhammed.arshad@norq.in>
2024-11-10 12:47:00 +0530

    Initail commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <div className='m-4'>
+            <div className="bg-[rgba(192,140,93,1)] h-96 w-full p-4 rounded-sm">
+                asjnajsdn
+            </div>
+        </div>
+    )
+}
+
+export default Footer
